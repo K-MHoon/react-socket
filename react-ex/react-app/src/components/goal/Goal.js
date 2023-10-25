3866ed383c034d9f8c2c2f5312d150d8da520340
K-MHOON <azmria117@gmail.com>
2023-10-25 21:07:43 +0900

    feat : Input, Goal 컴포넌트 추가

---
+++ react-ex/react-app/src/components/goal/Goal.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Goal.module.css";
+
+const Goal = ({ id, status, msg, onCheckChange }) => {
+    return (
+        <div className={styles.goalWrap}>
+            <label
+                className={status ? styles.textDisabled : styles.text}
+                htmlFor={id}
+            >
+                {status && <div className={styles.clean} />}
+                <input
+                    type="checkbox"
+                    id={id}
+                    name={id}
+                    data-msg={msg}
+                    onChange={onCheckChange}
+                    checked={status}
+                />
+                {msg}
+            </label>
+        </div>
+    );
+};
+
+export default Goal;
