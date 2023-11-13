771a3bb60e8b36c044ba476df7b4caac414dff67
K-MHOON <azmria117@gmail.com>
2023-11-13 19:25:05 +0900

    feat : 간단한 nodejs socket 통신 구현

---
+++ socket/net-module/client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+const socket = net.connect({ port: 5000 });
+socket.on("connect", () => {
+    console.log("connected to server!");
+
+    setInterval(() => {
+        socket.write("Hello.");
+    }, 1000);
+});
+
+socket.on("data", (chunk) => {
+    console.log("From Server:" + chunk);
+});
+
+socket.on("end", () => {
+    console.log("disconnected.");
+});
+socket.on("error", (err) => {
+    console.log(err);
+});
+socket.on("timeout", () => {
+    console.log("connection timeout.");
+});
