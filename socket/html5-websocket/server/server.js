f7d89f6e29964ca38c68e649116f372a22d2c9ce
K-MHOON <azmria117@gmail.com>
2023-11-16 21:14:50 +0900

    feat : Websocket 채팅 서비스 서버 구현

---
+++ socket/html5-websocket/server/server.js
@@ -0,0 +1,36 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 5000 });
+
+wss.on("connection", (ws) => {
+    const broadCastHandler = (msg) => {
+        wss.clients.forEach(function each(client, i) {
+            console.log(client);
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(msg);
+            }
+        });
+    };
+
+    ws.on("message", (res) => {
+        const { type, data, id } = JSON.parse(res);
+        switch (type) {
+            case "id":
+                broadCastHandler(
+                    JSON.stringify({ type: "welcome", data: data })
+                );
+                break;
+            case "msg":
+                broadCastHandler(
+                    JSON.stringify({ type: "other", data: data, id: id })
+                );
+                break;
+            default:
+                break;
+        }
+    });
+
+    ws.on("close", () => {
+        console.log("client has disconnected");
+    });
+});
