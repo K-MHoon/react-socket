d85d461653b23727bda08d749b5ac63e9d23aeb6
K-MHOON <azmria117@gmail.com>
2023-12-13 19:11:31 +0900

    feat : 라우팅 설정 추가

--- slack/client/src/App.js
+++ slack/client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { StoreProvider } from "./context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { IndexContainer, MainContainer } from "./containers";
 
 const App = () => {
-    return <div></div>;
+    return (
+        <StoreProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<IndexContainer />} />
+                    <Route path="/main" element={<MainContainer />} />
+                </Routes>
+            </Router>
+        </StoreProvider>
+    );
 };
 
 export default App;
